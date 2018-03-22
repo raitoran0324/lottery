2b04af2607dbc48b00880d3408446811dbfc4773
m305-61\w401 <A106082014@mail.shu.edu.tw>
2018-03-22 15:53:22 +0800

    初設定

---
+++ LOTTERY/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOTTERY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 宣告一個亂數並給予範圍
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(0, 100));
+
+            Console.ReadLine();
+        }
+    }
+}
